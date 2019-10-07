95a86e91edbac0c1673e19fd4ce2c4c0ea027a37
Yu Jiageng <13260035581@163.com>
2019-10-07 21:13:00 +0800

    time compute

--- examples/resnet20/resnet20.cpp
+++ examples/resnet20/resnet20.cpp
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <memory>
+#include <sys/time.h>
 #include "include/core/executor.hpp"
 
 int main(void)
@@ -15,18 +16,29 @@ int main(void)
     std::string input_name("test_batch.bin");
     int n_batch = 500;
     int top_5 = 5;
+    int top_1 = 1;
     RVTensor::Executor::sptr sp =
             RVTensor::Executor::create(model_name, n_batch);
 
     sp->parseModel();
+
+    struct timeval start, end;
+    int timeuse;
+    gettimeofday(&start, NULL);
 
     sp->loadImage(input_name, 10000, 32, 32, 3);
     for (int i = 0; i < 10000/n_batch; i++) {
         sp->compute(i);
     }
 
+    gettimeofday(&end, NULL);
+    timeuse = 1000000 * ( end.tv_sec - start.tv_sec  ) + end.tv_usec - start.tv_usec;
+    printf("inference time is %.3fs\n", (double)timeuse/1000000);
+
     int acc = sp->inferenceResult(top_5);
     printf("top5 acc:%d%%\n", acc/100);
+    acc = sp->inferenceResult(top_1);
+    printf("top1 acc:%d%%\n", acc/100);
 
     return 0;
 }
